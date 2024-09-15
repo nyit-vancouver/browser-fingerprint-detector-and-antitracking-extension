6fa536dd0428fad8fc13393cbbfb61e967df80aa
kkaylie <kkaylie778@gmail.com>
2024-09-15 16:19:56 -0700

    chore: data template

--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { getBroswerInfos } from '@/utils/getInfos'
+import {
+  getBroswerInfos,
+  getLocationInfos,
+  getHardwareInfos,
+  getGeneralInfos
+} from '@/utils/getInfos'
 
 type UaType = Record<string, any>
 
@@ -9,20 +14,59 @@ function Info() {
     device: { type: '-', vendor: '-' },
     engine: { name: '-', version: '-' },
     os: { name: '-', version: '-' },
-    cpu: { architecture: '-' }
+    cpu: { architecture: '-' },
+    plugins: []
+  })
+  const [location, setLocation] = useState({
+    longtitute: 0,
+    latitude: 0,
+    timezone: '',
+    localTime: '',
+    ip: '',
+    isp: '',
+    geocode: '',
+    region: '',
+    city: ''
+  })
+  const [hardware, setHardware] = useState({
+    canvas: '',
+    webGL: '',
+    audio: '',
+    video: '',
+    screenSize: '',
+    resolution: '',
+    colorDepth: ''
+  })
+  const [general, setGeneral] = useState({
+    language: '',
+    fonts: '',
+    cpu: '',
+    cookie: '',
+    header: {
+      DNT: '',
+      referrer: '',
+      acceptLanguage: ''
+    }
   })
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await getBroswerInfos()
-      setUa(res)
+      const ua = await getBroswerInfos()
+      setUa(ua)
+      const location = await getLocationInfos()
+      setLocation(location)
+      const hardware = await getHardwareInfos()
+      setHardware(hardware)
+      const general = await getGeneralInfos()
+      setGeneral(general)
     }
     fetchData()
   }, [])
-
+  // TODO: 优化页面展示
   return (
     <div>
-      <h1 className="mt-4">Info</h1>
+      <h1 className="mt-4">Info:</h1>
+      <h2 className="mt-4">Browser:</h2>
       <p>{ua.browser.name}</p>
       <p>{ua.browser.version}</p>
       <p>{ua.device.type}</p>
@@ -32,6 +76,40 @@ function Info() {
       <p>{ua.os.name}</p>
       <p>{ua.os.version}</p>
       <p>{ua.cpu.architecture}</p>
+      <h2 className="mt-4">Plugins:</h2>
+      <ul>
+        {ua.plugins?.map((item: Plugin) => (
+          <li key={item.name}>
+            {item.name}: {item.description}
+          </li>
+        ))}
+      </ul>
+      <h2 className="mt-4">Location:</h2>
+      <p>{location.longtitute}</p>
+      <p>{location.latitude}</p>
+      <p>{location.timezone}</p>
+      <p>{location.localTime}</p>
+      <p>{location.ip}</p>
+      <p>{location.isp}</p>
+      <p>{location.geocode}</p>
+      <p>{location.region}</p>
+      <p>{location.city}</p>
+      <h2 className="mt-4">Hardware:</h2>
+      <p>{hardware.canvas}</p>
+      <p>{hardware.webGL}</p>
+      <p>{hardware.audio}</p>
+      <p>{hardware.video}</p>
+      <p>{hardware.screenSize}</p>
+      <p>{hardware.resolution}</p>
+      <p>{hardware.colorDepth}</p>
+      <h2 className="mt-4">General:</h2>
+      <p>{general.language}</p>
+      <p>{general.fonts}</p>
+      <p>{general.cpu}</p>
+      <p>{general.cookie}</p>
+      <p>{general.header.DNT}</p>
+      <p>{general.header.referrer}</p>
+      <p>{general.header.acceptLanguage}</p>
     </div>
   )
 }
--- src/utils/getInfos.ts
+++ src/utils/getInfos.ts
@@ -3,6 +3,12 @@ import { UAParser } from 'ua-parser-js'
 export async function getBroswerInfos() {
   const parser = new UAParser()
   const res = parser.getResult()
+  const plugins =
+    Object.values(navigator.plugins)?.map((item) => ({
+      name: item.name,
+      description: item.description
+    })) || []
+
   if (navigator.userAgentData) {
     const ua = await navigator.userAgentData.getHighEntropyValues([
       'platform',
@@ -30,11 +36,53 @@ export async function getBroswerInfos() {
       },
       cpu: {
         architecture: ua.architecture
-      }
+      },
+      plugins
     }
   }
-  return res
+  return { ...res, plugins }
 }
-export function getLocationInfos() {}
-export function getHardwareInfos() {}
-export function getSoftwareInfos() {}
+
+export async function getLocationInfos() {
+  // TODO: 地理位置经纬度、时区、当地时间、IP地址、ISP、地理编码、地区城市、国家
+  // TODO: webRTC、Geolocation API、3rd party service
+
+  return {
+    longtitute: 0,
+    latitude: 0,
+    timezone: '',
+    localTime: '',
+    ip: '',
+    isp: '',
+    geocode: '',
+    region: '',
+    city: ''
+  }
+}
+export async function getHardwareInfos() {
+  // TODO: canvas、webGL、audio、video、screen size and resolution、color depth、
+  return {
+    canvas: '',
+    webGL: '',
+    audio: '',
+    video: '',
+    screenSize: '',
+    resolution: '',
+    colorDepth: ''
+  }
+}
+export async function getGeneralInfos() {
+  // TODO: 语言、字体、CPU、cookie、header（DNT、DoNotTrack、referrer、接收字体）
+  return {
+    language: '',
+    fonts: '',
+    cpu: '',
+    cookie: '',
+    header: {
+      // TODO: other headers
+      DNT: '',
+      referrer: '',
+      acceptLanguage: ''
+    }
+  }
+}
