627f91218929183b15bca38cd39a6624d5768c79
kkaylie <kkaylie778@gmail.com>
2024-09-15 14:31:44 -0700

    feat: getBroswerInfos

--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -1,12 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getBroswerInfos } from '@/utils/getInfos'
 
+type UaType = Record<string, any>
+
 function Info() {
-  const [browserInfo] = useState(getBroswerInfos())
+  const [ua, setUa] = useState<UaType>({
+    browser: { name: '-', version: '-' },
+    device: { type: '-', vendor: '-' },
+    engine: { name: '-', version: '-' },
+    os: { name: '-', version: '-' },
+    cpu: { architecture: '-' }
+  })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getBroswerInfos()
+      setUa(res)
+    }
+    fetchData()
+  }, [])
+
   return (
     <div>
       <h1 className="mt-4">Info</h1>
-      <p>{browserInfo}</p>
+      <p>{ua.browser.name}</p>
+      <p>{ua.browser.version}</p>
+      <p>{ua.device.type}</p>
+      <p>{ua.device.vendor}</p>
+      <p>{ua.engine.name}</p>
+      <p>{ua.engine.version}</p>
+      <p>{ua.os.name}</p>
+      <p>{ua.os.version}</p>
+      <p>{ua.cpu.architecture}</p>
     </div>
   )
 }
--- src/utils/getInfos.ts
+++ src/utils/getInfos.ts
@@ -1,5 +1,39 @@
-export function getBroswerInfos() {
-  return navigator.userAgent
+import { UAParser } from 'ua-parser-js'
+
+export async function getBroswerInfos() {
+  const parser = new UAParser()
+  const res = parser.getResult()
+  if (navigator.userAgentData) {
+    const ua = await navigator.userAgentData.getHighEntropyValues([
+      'platform',
+      'platformVersion',
+      'architecture',
+      'model',
+      'uaFullVersion'
+    ])
+    return {
+      ...res,
+      browser: {
+        ...res.browser,
+        version:
+          ua.uaFullVersion ||
+          ua.fullVersionList?.join(',') ||
+          res.browser.version
+      },
+      device: {
+        ...res.device,
+        model: ua.model
+      },
+      os: {
+        ...res.os,
+        version: ua.platformVersion
+      },
+      cpu: {
+        architecture: ua.architecture
+      }
+    }
+  }
+  return res
 }
 export function getLocationInfos() {}
 export function getHardwareInfos() {}
