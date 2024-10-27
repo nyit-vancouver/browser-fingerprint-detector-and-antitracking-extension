b96860f5716f64f041071baa43dfc478b4ec061f
kkaylie <kkaylie778@gmail.com>
2024-10-27 12:04:46 -0700

    fix: chrome runtime error

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,9 +1,10 @@
 console.log('[content] loaded ')
-
 ;(function () {
-  const script = document.createElement('script')
-  script.src = chrome.runtime.getURL('static/js/inject.js')
-  document.documentElement.appendChild(script)
-  script.onload = () => script.remove()
+  if (chrome.runtime) {
+    const script = document.createElement('script')
+    script.src = chrome.runtime.getURL('static/js/inject.js')
+    document.documentElement.appendChild(script)
+    script.onload = () => script.remove()
+  }
 })()
 export {}
